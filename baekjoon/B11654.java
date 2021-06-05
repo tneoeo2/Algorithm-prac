7a7522cc5f4c749deb47fe4967d5bbd169323693
tneoeo2 <63712295+tneoeo2@users.noreply.github.com>
2021-06-05 16:10:43 +0900

    Create B11654.java

---
+++ baekjoon/B11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char toASCII = sc.next().charAt(0);
+		
+		System.out.println((int)toASCII);	//문자 입력시 정수 출력
+ 		
+		
+		
+		
+	}
+}
